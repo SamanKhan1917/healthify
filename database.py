a8985250ef1e870551665e251d3e5bf2b9e0f78e
SamanKhan1917 <111567300+SamanKhan1917@users.noreply.github.com>
2024-10-02 12:33:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,17 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # You can change this to a real DB URL
+DATABASE_URL = "mysql+pymysql://samankhan198:lenovoide@pad330S>@localhost/healthify"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
