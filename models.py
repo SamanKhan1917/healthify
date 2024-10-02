bb419ebc84c234fac4fc0955810200643164a891
SamanKhan1917 <111567300+SamanKhan1917@users.noreply.github.com>
2024-10-02 12:33:33 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,26 +1,24 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from .database import Base
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), unique=True, index=True)
+    password = Column(String(100))
 
 class MenuItem(Base):
     __tablename__ = "menu_items"
     
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    name = Column(String(100))
     price = Column(Float)
-
-class CartItem(Base):
-    __tablename__ = "cart_items"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    item_id = Column(Integer, ForeignKey("menu_items.id"))
-    user = Column(String(100))  # Optional user identifier
-    quantity = Column(Integer)
-    menu_item = relationship("MenuItem")
+    image_url = Column(String(255))
 
 class ContactMessage(Base):
     __tablename__ = "contact_messages"
     
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String(100))
     message = Column(String)
